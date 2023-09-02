7d0df025a5004a3cabe595911e5462674b393d61
Luna Lopis <lunalopis59@gmail.com>
2023-09-02 16:24:35 +0200

    show dettaglio

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,10 +10,29 @@ class PostController extends Controller
 {
     public function index(){
         // $posts= Post::all();
+            // Recupera una lista di post paginata con relazioni "type" e "tecnologies"
         $posts = Post::with('type', 'tecnologies')->paginate(3);
+         // Restituisci una risposta JSON con i risultati della query
         return response()->json([
             'success' => true,
             'results' => $posts,
         ]);
+    }
+
+
+    public function show($slug){
+        $post = Post::with('type', 'tecnologies')->where('slug', $slug)->first();
+        if ($post) {
+            return response()->json([
+                'success' => true,
+                'results' => $post,
+            ]);
+        }
+        else{
+            return response()->json([
+                'success' => false,
+                'results' => 'non trovato',
+            ]); 
+        }
     }
 }
